d8edd04f91d639a1f2b28b86f140b053b7a8b847
PKUfjh <2001110077@pku.edu.cn>
2021-10-07 12:19:31 +0800

    global parameters

--- hw3/parameters.cpp
+++ hw3/parameters.cpp
@@ -6,3 +6,12 @@ using namespace std;
 
 int natoms = 0; int neighbor_n = 0; double r_cut = 0; double extra_cut = 0; string geo_path = "";
 
+Atom *atoms = new Atom[900];
+
+void Atom::setpos(double position[3]){
+    memcpy(pos,position,sizeof(pos));
+}
+
+void Atom::setvel(double velocity[3]){
+    memcpy(vel,velocity,sizeof(vel));
+}
--- hw3/parameters.h
+++ hw3/parameters.h
@@ -8,4 +8,21 @@ using namespace std;
 extern int natoms;extern int neighbor_n;extern double r_cut;extern double extra_cut;extern string geo_path;
 
 
+class Atom
+{
+private:
+public:
+    string ID ;
+    int index;
+    int nei_num;
+    int *nei_list;
+    double pos[3];
+    double vel[3];
+    // declare function
+    void setpos(double position[3]);
+    void setvel(double velocity[3]);
+};
+
+extern Atom *atoms;
+
 #endif
