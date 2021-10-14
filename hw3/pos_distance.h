2b27217bb320b9d2357ca3002af5a20ac0bd4c4d
PKUfjh <2001110077@pku.edu.cn>
2021-10-14 16:18:21 +0800

    fix bugs in linux

--- hw3/pos_distance.h
+++ hw3/pos_distance.h
@@ -2,6 +2,9 @@
 #define POS_DISTANCE_H
 
 //compute the distance between two coordinates
+double single_distance(double alist[3], double blist[3]);
+
+//compute the distance between two coordinates under PBC
 double pos_distance(double alist[3], double blist[3]);
 
 #endif
