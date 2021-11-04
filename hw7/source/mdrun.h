553fe0c85e73f822ebac606f932dc7e100668911
PKUfjh <2001110077@pku.edu.cn>
2021-11-04 21:51:00 +0800

    small fix

--- hw7/source/mdrun.h
+++ hw7/source/mdrun.h
@@ -1,9 +1,6 @@
 #ifndef MDRUN_H
 #define MDRUN_H
 
-//read in from previous MD run
-double **read_in(string filename,int STEP);
-
 /*molecular dynamics calculation, output atomic position, velocity, forces at given step, 
 also output the total energy, potential energy, kinetic energy and temperature at given step.*/
 void mdrun(int STEP);
