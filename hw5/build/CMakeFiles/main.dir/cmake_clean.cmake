01c05142c7e473709c7cb02458bd16edee63b01c
PKUfjh <2001110077@pku.edu.cn>
2021-11-04 15:16:45 +0800

    add cmake to hw5

---
+++ hw5/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/source/force_energy.cpp.o"
+  "CMakeFiles/main.dir/source/force_energy.cpp.o.d"
+  "CMakeFiles/main.dir/source/initialize.cpp.o"
+  "CMakeFiles/main.dir/source/initialize.cpp.o.d"
+  "CMakeFiles/main.dir/source/kin_energy.cpp.o"
+  "CMakeFiles/main.dir/source/kin_energy.cpp.o.d"
+  "CMakeFiles/main.dir/source/main.cpp.o"
+  "CMakeFiles/main.dir/source/main.cpp.o.d"
+  "CMakeFiles/main.dir/source/mdrun.cpp.o"
+  "CMakeFiles/main.dir/source/mdrun.cpp.o.d"
+  "CMakeFiles/main.dir/source/neighbor.cpp.o"
+  "CMakeFiles/main.dir/source/neighbor.cpp.o.d"
+  "CMakeFiles/main.dir/source/parameters.cpp.o"
+  "CMakeFiles/main.dir/source/parameters.cpp.o.d"
+  "CMakeFiles/main.dir/source/pos_distance.cpp.o"
+  "CMakeFiles/main.dir/source/pos_distance.cpp.o.d"
+  "CMakeFiles/main.dir/source/read_in.cpp.o"
+  "CMakeFiles/main.dir/source/read_in.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
